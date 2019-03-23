4705caa710c5d37d35efb6e50f837fcccf56b0cf
Ryan Zhang <ryanzhangau@gmail.com>
2019-03-23 12:47:56 +1100

    create helper file

---
+++ src/store/helper.js
@@ -0,0 +1,28 @@
+const MATH_OP_MAP = {
+  "√": "Math.sqrt(",
+  "^": "Math.pow("
+};
+
+const MATH_END_MAP = {
+  "Math.sqrt(": ")",
+  "Math.pow(": ",2)"
+};
+
+export const formulaStartHelper = operator => {
+  return `${MATH_OP_MAP[operator]}`;
+};
+
+export const formularEndHelper = operator => {
+  return `${MATH_END_MAP[operator]}`;
+};
+
+export const calcHelper = formula => {
+  let res = "";
+  try {
+    res = eval(formula).toString();
+  } catch (e) {
+    res = "Bad Expression";
+    console.error(`${formula} is a bad expession, error: ${e}`);
+  }
+  return res;
+};
