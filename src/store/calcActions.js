177c2b71fcd5512df0d96865edd398ed68bf8911
Ryan Zhang <ryanzhangau@gmail.com>
2019-03-23 12:48:11 +1100

    button actions

--- src/store/calcActions.js
+++ src/store/calcActions.js
@@ -1,3 +1,11 @@
-export const add = number => {
-  return { type: "ADD", payload: number };
+export const typing = button => {
+  return { type: "TYPING", payload: button };
+};
+
+export const clear = () => {
+  return { type: "CLEAR", payload: "" };
+};
+
+export const calc = () => {
+  return { type: "CALC", payload: "" };
 };
